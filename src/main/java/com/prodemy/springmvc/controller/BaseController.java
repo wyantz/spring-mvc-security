9140ea4ca9dfdc7ffc340c1ac853fc0c27dc75b7
awiyanto.ajisasongko@gmail.com <awiyanto.ajisasongko@gmail.com>
2022-10-04 10:00:07 +0700

    Menambah method encrypt

--- src/main/java/com/prodemy/springmvc/controller/BaseController.java
+++ src/main/java/com/prodemy/springmvc/controller/BaseController.java
@@ -3,18 +3,26 @@
  */
 package com.prodemy.springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author wyant
  *
  */
 public class BaseController {
+	@Autowired protected BCryptPasswordEncoder encoder;
+	
 	protected boolean hasRole(String role, Authentication auth) {
 		for (GrantedAuthority a : auth.getAuthorities()) {
 			if (a.getAuthority().equals(role)) return true;
 		}
 		return false;
+	}
+	
+	protected String encrypt(String text) {
+		return encoder.encode(text);
 	}
 }
